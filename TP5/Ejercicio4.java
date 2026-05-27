317196476d9e02328e41965a8a62cc3e8dbf752c
ampirosell <129613071+ampirosell@users.noreply.github.com>
2026-05-27 18:34:43 -0300

    Actualizar Ejercicio4.java

--- TP5/Ejercicio4.java
+++ TP5/Ejercicio4.java
@@ -2,4 +2,32 @@ package proyectos.TP5;
 
 public class Ejercicio4{
 
-}
+    public List<List<Integer>> ejercicio5(int[] numeros){
+        List<List<Integer>> result = new ArrayList<>();
+
+        int[] posiciones = new int[];
+
+        this.buscarSubconjuntosDisjuntos(numeros, posiciones, 0, 0, 0);
+
+        //result.add??
+        return result;
+    }
+
+
+    private void buscarSubconjuntosDisjuntos(int[] numeros, int[] posiciones,
+        int suma1, int suma2, int index){
+        
+        if(index==numeros.size())
+            return;
+
+        if(numeros==null)
+            return;
+        
+        if(numeros.size()<=posiciones.size()){
+            return;
+        }
+        
+
+
+
+    }
