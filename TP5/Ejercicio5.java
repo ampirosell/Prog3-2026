f46b71738fd28d1a197659bb0ed70a208b8e8d8e
ampirosell <129613071+ampirosell@users.noreply.github.com>
2026-05-27 19:03:43 -0300

    Crear Ejercicio5.java

---
+++ TP5/Ejercicio5.java
@@ -0,0 +1,21 @@
+package proyectos.TP5;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Ejercicio5{
+
+/*  Ejercicio 5‬
+‭ Asignación de tareas a procesadores. Se tienen m procesadores (procesadorId) idénticos y n tareas (tareaName) con un tiempo‬
+‭ de ejecución dado. Se requiere encontrar una asignación de tareas a procesadores de manera de‬
+minimizar el tiempo de ejecución del total de tareas.‬
+‭*/
+
+    public Map<Integer,List<String>> ejercicio5(int[] procesadoresId, 
+        String[] tareasNames){
+
+        return null;
+    }
+
+
+}
